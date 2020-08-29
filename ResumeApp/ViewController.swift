60a520e9af4d97932462b394ac1c295cb92641b0
Aleksandr Lukash <a.lukash.92@gmail.com>
2020-08-29 17:23:29 +0300

    Initial Commit

---
+++ ResumeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResumeApp
+//
+//  Created by Aleksandr on 29.08.2020.
+//  Copyright © 2020 Aleksandr Lukash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
